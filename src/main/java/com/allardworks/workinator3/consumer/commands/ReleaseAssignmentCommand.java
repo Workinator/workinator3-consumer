47f9cc928aa398d55a954e3b7e468385d86c5fd5
Jay Allard <jay_allard@ultimatesoftware.com>
2018-03-25 00:54:34 -0400

    breaking into multiple projects

--- src/main/java/com/allardworks/workinator3/consumer/commands/ReleaseAssignmentCommand.java
+++
@@ -1,9 +0,0 @@
-package com.allardworks.workinator3.consumer.commands;
-
-import com.allardworks.workinator3.consumer.Assignment;
-import lombok.Data;
-
-@Data
-public class ReleaseAssignmentCommand {
-    private final Assignment assignment;
-}
